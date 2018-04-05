4a1aa12ba9ef7411002da370799d96fdf0284706
Liviavil <livia.crescenzo@gmail.com>
2018-04-05 11:54:39 +0200

    aa

---
+++ AmministrazioneAsdep/DAL/IProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmministrazioneAsdep.DAL
+{
+    public interface IProvider<T> where T : class
+    {
+        List<T> GetAll(AmministrazioneAsdepEntities db);
+
+        T SelectById(AmministrazioneAsdepEntities db, string id);
+
+        List<T> Find(AmministrazioneAsdepEntities db, T obj);
+
+        int AddOne(AmministrazioneAsdepEntities db, T obb);
+
+        int AddMany(AmministrazioneAsdepEntities db, List<T> objs);
+
+        int Update(AmministrazioneAsdepEntities db, T obj);
+
+    }
+}
---
+++ AmministrazioneAsdep/DAL/SoggettiImportAppoggioProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmministrazioneAsdep.DAL
+{
+    public class SoggettiImportAppoggioProvider
+    {
+
+        public static List<SoggettiImportAppoggio> GetAllAssicurati(AmministrazioneAsdepEntities db)
+        {
+            List<SoggettiImportAppoggio> _assicurati = new List<SoggettiImportAppoggio>();
+            _assicurati = (from table in db.SoggettiImportAppoggio select table).ToList();
+
+            return _assicurati;
+        }
+
+        public static int AddAssicurato(AmministrazioneAsdepEntities db, SoggettiImportAppoggio assicurato)
+        {
+            int result = -1;
+            try
+            {
+                db.SoggettiImportAppoggio.Add(assicurato);
+                result = db.SaveChanges();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+        public static int AddAssicurati(AmministrazioneAsdepEntities db, List<SoggettiImportAppoggio> assicurati)
+        {
+            int result = -1;
+            try
+            {
+                foreach (SoggettiImportAppoggio _ass in assicurati)
+                {
+                    db.SoggettiImportAppoggio.Add(_ass);
+                    result = db.SaveChanges();
+                }
+
+                return result;
+            }
+            catch (Exception ex) { return -1; }
+        }
+
+    }
+}
